8a48d037defc35d875e1a730316726c27dca8bda
Alex0775edu <aaditya0775@gmail.com>
2026-03-29 22:45:43 +0530

    fix(migrations): make attendance removal safe

--- attendance/migrations/0002_alter_attendance_unique_together_and_more.py
+++ attendance/migrations/0002_alter_attendance_unique_together_and_more.py
@@ -1,7 +1,38 @@
 # Restored migration file to match applied DB history.
 
 import django.db.models.deletion
+from django.core.exceptions import FieldDoesNotExist
 from django.db import migrations, models
+
+
+class SafeRemoveField(migrations.RemoveField):
+    """Remove a field only if it exists in the migration state/database."""
+
+    def state_forwards(self, app_label, state):
+        model_key = (app_label, self.model_name_lower)
+        model_state = state.models.get(model_key)
+        if not model_state:
+            return
+        field_names = {name for name, _ in model_state.fields}
+        if self.name not in field_names:
+            return
+        super().state_forwards(app_label, state)
+
+    def database_forwards(self, app_label, schema_editor, from_state, to_state):
+        from_model = from_state.apps.get_model(app_label, self.model_name)
+        try:
+            from_model._meta.get_field(self.name)
+        except FieldDoesNotExist:
+            return
+        super().database_forwards(app_label, schema_editor, from_state, to_state)
+
+    def database_backwards(self, app_label, schema_editor, from_state, to_state):
+        to_model = to_state.apps.get_model(app_label, self.model_name)
+        try:
+            to_model._meta.get_field(self.name)
+        except FieldDoesNotExist:
+            return
+        super().database_backwards(app_label, schema_editor, from_state, to_state)
 
 
 class Migration(migrations.Migration):
@@ -12,11 +43,11 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
+        SafeRemoveField(
             model_name='attendance',
             name='teacher_assignment',
         ),
-        migrations.RemoveField(
+        SafeRemoveField(
             model_name='attendance',
             name='remarks',
         ),
